e1ef146a104afb84d48d0276f886176fd466bd57
hari <harisaranmishra.hari@gmail.com>
2024-10-19 21:21:46 +0530

    firstcommit

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
+};
+
