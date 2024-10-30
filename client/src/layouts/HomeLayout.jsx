2c01d1905ab0259aa069185f4f72749b39643b98
hari <harisaranmishra.hari@gmail.com>
2024-10-30 22:00:54 +0530

    modified

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -3,7 +3,7 @@ import { FiMenu } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import { logout } from '../redux/slices/authSlice';
 
 
